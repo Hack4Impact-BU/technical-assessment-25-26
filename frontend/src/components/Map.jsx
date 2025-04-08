9c4cacd299819ed07438b6ea0eb93388786a9baf
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-04-08 19:14:30 -0400

    added popup component from documentation

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,4 +1,31 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import React, { useState } from "react";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+} from "react-leaflet";
+
+function LocationMarker() {
+  const [position, setPosition] = useState(null);
+
+  useMapEvents({
+    click() {
+      this.locate();
+    },
+    locationfound(e) {
+      setPosition(e.latlng);
+      this.flyTo(e.latlng, this.getZoom());
+    },
+  });
+
+  return position === null ? null : (
+    <Marker position={position}>
+      <Popup>You are here</Popup>
+    </Marker>
+  );
+}
 
 const Map = () => {
   return (
@@ -12,11 +39,7 @@ const Map = () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          Popup
-        </Popup>
-      </Marker>
+      <LocationMarker />
     </MapContainer>
   );
 };
