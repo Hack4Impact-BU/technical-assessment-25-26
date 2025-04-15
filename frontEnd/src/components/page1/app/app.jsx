00734a36c562db9ead7275f3eaea397a48d03d92
timmyjr11 <timmyytjr@gmail.com>
2025-04-15 00:15:04 -0400

    Map is now fucntional, next step set up backend for history tracking and intialize google AI for location facts

--- frontEnd/src/components/page1/app/app.jsx
+++ frontEnd/src/components/page1/app/app.jsx
@@ -1,16 +1,29 @@
-import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import './app.css'
-
+import React from 'react'
 
 function App() {
     return (
     <>
-            <div id="projects">
-                <h2>Projects</h2>
-            </div>
-            <div id="exp">
-                <h2>Experiences</h2>
-            </div>
+        <div id="projects">
+            <h2>Projects</h2>
+        </div>
+
+        <MapContainer center={[51.505, -0.09,]} zoom={13} scrollWheelZoom={false}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[51.505, -0.09]}>
+                <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+            </Marker>
+        </MapContainer>
+
+        <div id="exp">
+            <h2>Experiences</h2>
+        </div>
     </>
     )
 }
--- frontEnd/src/main.jsx
+++ frontEnd/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './components/page1/app/app.jsx'
+import Map from './components/page1/map/map.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
