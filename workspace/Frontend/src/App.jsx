f9f9cbc020645e2b36ed04fb66af6da4bf2e86e0
zohaibm08 <zohaibmohammed30@gmail.com>
2025-04-20 02:54:42 -0400

    Initial setup attempt

---
+++ workspace/Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/navbar/navbar";
+import Map from "./components/map/map";
+
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <Navbar/>
+            <Map/>
+        </>
+    )
+}  
+
+export default App;
---
+++ workspace/Frontend/src/components/navbar/navbar.jsx
@@ -0,0 +1,13 @@
+import './navbar.css';
+
+function Navbar() {
+    return (
+        <div id = "navbar">
+            <h1>ChronoZone</h1>
+            <a href = "#Map">Map</a>
+            <a href = "#History">History</a>
+        </div>
+    )
+}
+
+export default Navbar;
