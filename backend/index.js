6c251227f7fc71b4bac7de78377947475fad6351
Trevor Kerxhalli <trevorkerxhalli@gmail.com>
2025-04-25 19:48:22 -0400

    changed port for sunriseAPI to 5050

--- backend/index.js
+++ backend/index.js
@@ -33,5 +33,5 @@ app.get('/', (req, res) => {
   res.send('Sol Atlas Backend Running');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
