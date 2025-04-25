c4107e08383f669345d8d31782f7f219cd1f3909
Nicholas Zhang <nzhan01@bu.edu>
2025-04-25 19:02:49 -0400

    added coded to correctly render leaflet marker icons

--- workspace/frontend/src/components/leaflet/leaflet.jsx
+++ workspace/frontend/src/components/leaflet/leaflet.jsx
@@ -3,6 +3,19 @@ import {useState, } from "react";
 import './leaflet.css'
 import 'leaflet/dist/leaflet.css'
 
+import L from 'leaflet'
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
+import markerIcon from 'leaflet/dist/images/marker-icon.png'
+import markerShadow from 'leaflet/dist/images/marker-shadow.png'
+
+// Fix for missing marker icons on production build
+delete L.Icon.Default.prototype._getIconUrl
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: markerIcon2x,
+    iconUrl: markerIcon,
+    shadowUrl: markerShadow,
+})
 
 
 
