4a6e8f7f4a42b50fd86f2219b5cbf6a89e2a0ecd
Trevor Kerxhalli <trevorkerxhalli@gmail.com>
2025-04-25 18:41:57 -0400

    added a comma in headers

--- frontend/src/api/sunriseAPI.js
+++ frontend/src/api/sunriseAPI.js
@@ -2,7 +2,7 @@ export async function getSunriseSunset(lat, lng) {
     const response = await fetch('/api/sunrise', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({ lat, lng })
     });
