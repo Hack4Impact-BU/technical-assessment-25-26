48f94d5677f942ce7fcc2ff93fa58a7a07f1e24f
Trevor Kerxhalli <trevorkerxhalli@gmail.com>
2025-04-25 19:46:27 -0400

    changed to port 5050

--- hello-world-backend/index.js
+++ hello-world-backend/index.js
@@ -6,5 +6,5 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
