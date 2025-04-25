1326e3dcb517d4491da240a1fbe892b4cf442bf5
Trevor Kerxhalli <trevorkerxhalli@gmail.com>
2025-04-25 16:56:38 -0400

    Created front end pages

---
+++ frontend/src/pages/History.jsx
@@ -0,0 +1,8 @@
+export default function History() {
+    return (
+      <div>
+        <h1>History</h1>
+        <p>Here you'll see locations you've tapped, along with their sunrise and sunset times.</p>
+      </div>
+    );
+  }
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <div>
+        <h1>Welcome to Sol Atlas</h1>
+        <p>Click anywhere on the map to discover sunrise and sunset times!</p>
+      </div>
+    );
+  }
