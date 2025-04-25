a946aa6f9b767e847b953b267d998c26f02d7674
Joonseomoon <jsmoon416@gmail.com>
2025-04-25 01:17:54 -0400

    rename from frontend/src/App.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import HomePage from './pages/HomePage';
+import HomePage from './pages/Homepage';
 import HistoryPage from './pages/HistoryPage';
 
 export default function App() {
