72617184df2944aac9419d55f1eb8fe5da21a46a
Joonseomoon <jsmoon416@gmail.com>
2025-04-25 15:21:27 -0400

    minor fixess

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,7 @@ import { GoogleGenerativeAI } from '@google/generative-ai';
 dotenv.config();
 
 const app = express();
+app.set('trust proxy', 1);
 app.use(cors());
 app.use(express.json());
 
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -4,6 +4,15 @@ import { v4 as uuidv4 } from 'uuid';
 import 'leaflet/dist/leaflet.css';
 import CurrentLocationPopup from '../components/CurrentLocationPopup';
 import SunPopup from '../components/SunPopup';
+import iconUrl from 'leaflet/dist/images/marker-icon.png';
+import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png';
+import shadowUrl from 'leaflet/dist/images/marker-shadow.png';
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl,
+    iconUrl,
+    shadowUrl,
+});
 
 export default function HomePage() {
     const [sessionId] = useState(() => {
