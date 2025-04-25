e40b377c46aee116deb4ea0cffa6e19d1b10fafb
Trevor Kerxhalli <trevorkerxhalli@gmail.com>
2025-04-25 18:14:05 -0400

    added a global logger to see if any requests are reaching the server

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log(`[${req.method}] ${req.url}`);
+  next();
+});
+
 // MongoDB (can be commented for now if unused)
 //mongoose.connect(process.env.MONGO_URI)
 //  .then(() => console.log('Connected to MongoDB'))
