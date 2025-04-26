d3e1c395e6ce63bde925d3ff3c472e0fcd2f09e5
Trevor Kerxhalli <trevorkerxhalli@gmail.com>
2025-04-25 20:37:09 -0400

    added DELETE to cors

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(cors({
   origin: 'http://localhost:3000',
-  methods: ['GET', 'POST', 'OPTIONS'],
+  methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type'],
 }));
 
