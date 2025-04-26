01ea4ab89a3a184ca7c3d644112a439ead01de04
Trevor Kerxhalli <trevorkerxhalli@gmail.com>
2025-04-25 21:31:13 -0400

    added models/ to ensures the correct path is constructed for the API URL

--- backend/utils/gemini.js
+++ backend/utils/gemini.js
@@ -3,7 +3,9 @@ import { GoogleGenerativeAI } from '@google/generative-ai';
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 export async function findSimilarSunTimesPlace(sunrise, sunset) {
-const model = genAI.getGenerativeModel({ model: 'gemini-1.5-pro-latest' });
+    const model = genAI.getGenerativeModel({
+        model: 'models/gemini-1.5-pro-latest'
+      });
 
   const prompt = `
   I have a sunrise time of ${sunrise} and a sunset time of ${sunset}.
