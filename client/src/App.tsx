a75d2b67585b981d6bcb8d5fff316428dd91503c
Eddie <edd1e20786@gmail.com>
2025-04-30 19:07:44 -0400

    added vercel analytics

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import History from './pages/History'
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
 
